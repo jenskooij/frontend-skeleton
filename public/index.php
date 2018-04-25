16aee4018175f95ab49d0c171fc56e2846481acd
Jens Kooij <jens.kooij@incentro.com>
2018-04-25 14:57:47 +0200

    Basic set up of frontend skeleton

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php include(__DIR__ . '/templates/base/header.php'); ?>
+  <div class="center-wrapper">
+      <?php include(__DIR__ . '/templates/componets/home.php'); ?>
+  </div>
+<?php include(__DIR__ . '/templates/base/footer.php'); ?>
